9525f6b5a7426ede7cee9675f49b7b18c407d20b
Hiru1728 <hirucse1728@gmail.com>
2022-11-15 15:15:46 +0600

    pass the data mongoDB

--- src/Pages/Home/Services/ShowService.js
+++ src/Pages/Home/Services/ShowService.js
@@ -1,9 +1,57 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
+import { AuthContext } from '../../../context/AuthProvider/AuthProvider';
+import ShowAllReview from './ShowAllReview/ShowAllReview';
 
 const ShowService = () => {
-    const service = useLoaderData();
-    const { picture, balance, _id, name, about } = service;
+    const { user } = useContext(AuthContext);
+    const [reviews, setReviews] = useState({});
+    const serviceShow = useLoaderData();
+    const { picture, balance, _id, name, about } = serviceShow;
+
+    const url = `https://assignment-eleven-heart-doctor-appointment-server.vercel.app/reviews?`;
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            setReviews(data)
+        })
+
+
+    const addReview = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const message = form.message.value;
+        const email = user?.email || 'unregistered';
+        const img = user?.photoURL || 'No image';
+
+
+        const review = {
+            service: _id,
+            serviceName: name,
+            email,
+            message,
+            img,
+        }
+
+        fetch('https://assignment-eleven-heart-doctor-appointment-server.vercel.app/reviews', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json',
+            },
+            body: JSON.stringify(review)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                form.reset();
+            })
+            .catch(error => console.error(error))
+    }
+
+
+
+
     return (
         <div className="card card-compact w-auto bg-base-100 shadow-xl">
             <figure><img src={picture} alt="Surgery" /></figure>
@@ -12,8 +60,27 @@ const ShowService = () => {
                 <h4>Coast: {balance}</h4>
                 <p>{about}</p>
                 <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
+                    <button className="btn btn-primary">Add Review</button>
                 </div>
+
+                {
+                    (user?.email) ?
+                        <form onSubmit={addReview}>
+                            <textarea name='message' className="textarea textarea-bordered h-24 w-full" placeholder="Your Message" required></textarea>
+                            <div className='card-actions justify-end'>
+                                <input className='btn btn-warning ' type="submit" value="Add Review" />
+                            </div>
+                        </form>
+                        :
+                        <div>
+                            {
+                                reviews.map(review => <ShowAllReview
+                                    key={review._id}
+                                    review={review}
+                                ></ShowAllReview>)
+                            }
+                        </div>
+                }
             </div>
         </div>
     );
--- src/Routers/Route/Route.js
+++ src/Routers/Route/Route.js
@@ -5,6 +5,7 @@ import Home from "../../Pages/Home/Home/Home";
 import AllService from "../../Pages/Home/Services/AllService";
 import ShowService from "../../Pages/Home/Services/ShowService";
 import Login from "../../Pages/Login/Login";
+import MyReview from "../../Pages/MyReview/MyReview";
 import SignUp from "../../Pages/SignUp/SignUp";
 
 const { createBrowserRouter } = require("react-router-dom");
@@ -30,6 +31,10 @@ const router = createBrowserRouter([
                 path: '/services/:id',
                 element: <ShowService></ShowService>,
                 loader: ({ params }) => fetch(`https://assignment-eleven-heart-doctor-appointment-server.vercel.app/services/${params.id}`)
+            },
+            {
+                path: '/myreviews',
+                element: <MyReview></MyReview>
             },
             {
                 path: '/signup',
